65a5c2035c1752a57c416a31142f1fb5f8d83410
Xz00zX <m.zohair00@gmail.com>
2022-07-07 17:18:32 +0500

    🧹 feat: queue-and-execute update

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -3,7 +3,7 @@ import { network, ethers } from "hardhat"
 import { proposalsFile, developmentChains, VOTING_PERIOD } from "../helper-hardhat-config"
 import { moveBlocks } from "../utils/move-blocks"
 
-const index = 0
+const index = 1
 
 async function main(proposalIndex: number) {
   const proposals = JSON.parse(fs.readFileSync(proposalsFile, "utf8"))
@@ -11,19 +11,20 @@ async function main(proposalIndex: number) {
   const proposalId = proposals[network.config.chainId!][proposalIndex]
   // 0 = Against, 1 = For, 2 = Abstain for this example
   const voteWay = 1
-  const reason = "I lika do da cha cha"
+  const reason = "[Reason for Voting]"
   await vote(proposalId, voteWay, reason)
 }
 
 // 0 = Against, 1 = For, 2 = Abstain for this example
 export async function vote(proposalId: string, voteWay: number, reason: string) {
   console.log("Voting...")
-  const governor = await ethers.getContract("VoterContract")
-  const voteTx = await governor.castVoteWithReason(proposalId, voteWay, reason)
-  const voteTxReceipt = await voteTx.wait(1)
+  let governor = await ethers.getContract("VoterContract")
+  let voteTx = await governor.castVoteWithReason(proposalId, voteWay, reason)
+  let voteTxReceipt = await voteTx.wait(1)
   console.log(voteTxReceipt.events[0].args.reason)
-  const proposalState = await governor.state(proposalId)
+  let proposalState = await governor.state(proposalId)
   console.log(`Current Proposal State: ${proposalState}`)
+
   if (developmentChains.includes(network.name)) {
     await moveBlocks(VOTING_PERIOD + 1)
   }
